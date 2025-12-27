c8eb3a1df46f1893ae82a4394963f85de9371300
Kranthi596rguktian <kranthimutyalapalli596@gmail.com>
2025-12-27 12:18:49 +0530

    my project initialization is done.

---
+++ get_lib_versions.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+packages = [
+    "ragas",
+    "langchain-mcp-adapters",
+    "mcp",
+    "ddgs",
+    "langchain-openai",
+    "langchain"
+    ]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
